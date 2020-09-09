e8b6ccad00188c9c928f0d356a366fd5178311d3
zhx446 <2553268591@qq.com>
2020-09-09 17:32:00 +0800

    9-9 17.31

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -19,33 +19,30 @@
 				<van-grid-item icon="photo-o" text="短视频" />
 			</van-grid>
 			<!-- 标签导航 -->
-			<van-tabs v-model="articleActive" :sticky="true" offset-top="65px" title-active-color="#000" title-inactive-color="#B8B8B8"
-			:swipeable="true" animated color="#2671FE">
+			<van-tabs v-model="articleActive" :sticky="true" offset-top="65px" title-active-color="#000" title-inactive-color="#B8B8B8" :swipeable="true" animated color="#2671FE">
 				<!-- 热门模块 -->
 				<van-tab title="热门" title-style="font-weight:600;">
 					<div class="article-module" v-for="item in articles" :key="item.id">
 						<div class="writer-module">
 							<div class="writer-info">
 								<van-image round width="30px" height="30px" src="https://img.yzcdn.cn/vant/cat.jpeg" />
-								<span>{{item.writerName}}</span>
+								<span>{{ item.writerName }}</span>
 							</div>
 							<div class="attention">+关注</div>
 						</div>
-						<div class="article-title">
-							{{item.articleTitle}}
-						</div>
+						<div class="article-title">{{ item.articleTitle }}</div>
 						<div class="article-content">
-							<div class="article-content-text">
-								{{item.contentText}}
-							</div>
+							<div class="article-content-text">{{ item.contentText }}</div>
 							<van-image width="180px" height="67px" fit="contain" src="https://img.yzcdn.cn/vant/cat.jpeg" />
 						</div>
 						<div class="article-info">
-							<div>{{item.articleTime}}</div>
+							<div>{{ item.articleTime }}</div>
 							<div>
 								<van-icon name="ellipsis" color="#ADADAD" size="large" />
-								<van-icon name="good-job-o" color="#505050" size="large" />{{item.likeNum}}
-								<van-icon name="chat-o" color="#505050" size="large" />{{item.commentNum}}
+								<van-icon name="good-job-o" color="#505050" size="large" />
+								{{ item.likeNum }}
+								<van-icon name="chat-o" color="#505050" size="large" />
+								{{ item.commentNum }}
 							</div>
 						</div>
 					</div>
@@ -65,7 +62,30 @@
 								<van-image width="110px" height="80px" fit="contain" src="https://img.yzcdn.cn/vant/cat.jpeg" />
 								<div class="my-discuss-bottom-right">
 									<div>上证指数讨论区</div>
-									<div><span>[99+条]</span>崩盘了，大家散了吧</div>
+									<div>
+										<span>[99+条]</span>
+										崩盘了，大家散了吧
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="wealth-team">
+							<div class="my-discuss-top">
+								<div>财富小组</div>
+								<div>
+									<span>全部</span>
+									<van-icon name="arrow" />
+								</div>
+							</div>
+							<div class="my-discuss-top my-wealt-bottom">
+								<van-image width="48%" height="48%" radius="12px 12px 12px 1px" fit="contain" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+								<van-image width="48%" height="48%" radius="12px 12px 12px 1px" fit="contain" src="https://img.yzcdn.cn/vant/cat.jpeg" />
+							</div>
+							<div class="my-discuss-bottom" v-for="item in wealthList" :key="item.id">
+								<van-image width="110px" height="80px" radius="12px 12px 12px 1px" fit="contain" :src="item.wealthImg" />
+								<div class="my-discuss-bottom-right">
+									<div>{{ item.wealthTitle }}</div>
+									<div>{{ item.wealthText }}</div>
 								</div>
 							</div>
 						</div>
@@ -78,229 +98,259 @@
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				value: '',
-				articleActive: 0,
-				articles: [{
-						writerName: '周海欣',
-						articleTitle: '9.8：科技和周期板块的打分贴',
-						contentText: '如预想的那样，今天还是在调整的，虽然目前看，大盘已经翻红了，貌似数据还挺不错的。',
-						articleTime: '10分钟前',
-						likeNum: 1000,
-						commentNum: 222
-
-					},
-					{
-						writerName: '周海欣',
-						articleTitle: '9.8：科技和周期板块的打分贴',
-						contentText: '如预想的那样，今天还是在调整的，虽然目前看，大盘已经翻红了，貌似数据还挺不错的。',
-						articleTime: '10分钟前',
-						likeNum: 1000,
-						commentNum: 222
-					},
-					{
-						writerName: '周海欣',
-						articleTitle: '9.8：科技和周期板块的打分贴',
-						contentText: '如预想的那样，今天还是在调整的，虽然目前看，大盘已经翻红了，貌似数据还挺不错的。',
-						articleTime: '10分钟前',
-						likeNum: 1000,
-						commentNum: 222
-					}
-				]
-			};
-		},
-		methods: {
-
-		},
-	};
+export default {
+	data() {
+		return {
+			value: '',
+			articleActive: 0,
+			articles: [
+				{
+					writerName: '周海欣',
+					articleTitle: '9.8：科技和周期板块的打分贴',
+					contentText: '如预想的那样，今天还是在调整的，虽然目前看，大盘已经翻红了，貌似数据还挺不错的。',
+					articleTime: '10分钟前',
+					likeNum: 1000,
+					commentNum: 222
+				},
+				{
+					writerName: '周海欣',
+					articleTitle: '9.8：科技和周期板块的打分贴',
+					contentText: '如预想的那样，今天还是在调整的，虽然目前看，大盘已经翻红了，貌似数据还挺不错的。',
+					articleTime: '10分钟前',
+					likeNum: 1000,
+					commentNum: 222
+				},
+				{
+					writerName: '周海欣',
+					articleTitle: '9.8：科技和周期板块的打分贴',
+					contentText: '如预想的那样，今天还是在调整的，虽然目前看，大盘已经翻红了，貌似数据还挺不错的。',
+					articleTime: '10分钟前',
+					likeNum: 1000,
+					commentNum: 222
+				}
+			],
+			wealthList: [
+				{
+					wealthImg: 'https://img.yzcdn.cn/vant/cat.jpeg',
+					wealthTitle: '黄龙大讲堂小组',
+					wealthText: '坚决看空，神机先看到3200'
+				},
+				{
+					wealthImg: 'https://img.yzcdn.cn/vant/cat.jpeg',
+					wealthTitle: '黄龙大讲堂小组',
+					wealthText: '坚决看空，神机先看到3200'
+				},
+				{
+					wealthImg: 'https://img.yzcdn.cn/vant/cat.jpeg',
+					wealthTitle: '黄龙大讲堂小组',
+					wealthText: '坚决看空，神机先看到3200'
+				},
+				{
+					wealthImg: 'https://img.yzcdn.cn/vant/cat.jpeg',
+					wealthTitle: '黄龙大讲堂小组',
+					wealthText: '坚决看空，神机先看到3200'
+				}
+			]
+		};
+	},
+};
 </script>
 
 <style>
-	/*********************** 搜索导航栏 start ***********************/
-	.top-nav {
-		padding-top: 15px;
-	}
+/*********************** 搜索导航栏 start ***********************/
+.top-nav {
+	padding-top: 15px;
+}
 
-	.community-top-icon {
-		line-height: 37.6px;
-	}
+.community-top-icon {
+	line-height: 37.6px;
+}
 
-	.nav-module {
-		width: 100%;
-		position: fixed;
-		top: 0;
-		z-index: 1;
-	}
+.nav-module {
+	width: 100%;
+	position: fixed;
+	top: 0;
+	z-index: 1;
+}
 
-	/*********************** 搜索导航栏 end ***********************/
+/*********************** 搜索导航栏 end ***********************/
 
-	/*********************** 文章模块 start ***********************/
-	.community-content {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		margin-bottom: 85px;
-		margin-top: 65px;
-	}
+/*********************** 文章模块 start ***********************/
+.community-content {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	margin-bottom: 85px;
+	margin-top: 65px;
+}
 
-	/*********** 热门 start ***********/
+/*********** 热门 start ***********/
 
-	.article-module {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		border-bottom: 8px solid #F6F5FB;
-		padding-bottom: 12px;
-	}
+.article-module {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	border-bottom: 8px solid #f6f5fb;
+	padding-bottom: 12px;
+}
 
-	.writer-module {
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-		margin: 18px 10px;
-	}
+.writer-module {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	margin: 18px 10px;
+}
 
-	.writer-info {
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-		align-items: center;
-	}
+.writer-info {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+}
 
-	.writer-info span {
-		font-size: 16px;
-		font-weight: bold;
-		margin-left: 8px;
-	}
+.writer-info span {
+	font-size: 16px;
+	font-weight: bold;
+	margin-left: 8px;
+}
 
-	.attention {
-		background-color: #ECF7FE;
-		color: #5DADCC;
-		font-size: 13px;
-		width: 35px;
-		height: 25px;
-		padding: 0px 10px;
-		border-radius: 4px;
-		line-height: 25px;
-	}
+.attention {
+	background-color: #ecf7fe;
+	color: #5dadcc;
+	font-size: 13px;
+	width: 35px;
+	height: 25px;
+	padding: 0px 10px;
+	border-radius: 4px;
+	line-height: 25px;
+}
 
-	.article-content {
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-	}
+.article-content {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+}
 
-	.article-title {
-		font-size: 20px;
-		font-weight: bold;
-		text-align: left;
-		margin-left: 8px;
-		margin-bottom: 12px;
-	}
+.article-title {
+	font-size: 20px;
+	font-weight: bold;
+	text-align: left;
+	margin-left: 8px;
+	margin-bottom: 12px;
+}
 
-	.article-content-text {
-		font-size: 16px;
-		text-align: left;
-		margin-left: 10px;
-		width: 100%;
-	}
+.article-content-text {
+	font-size: 16px;
+	text-align: left;
+	margin-left: 10px;
+	width: 100%;
+}
 
-	.article-info {
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-		align-items: center;
-		margin: 8px 8px;
-	}
+.article-info {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+	margin: 8px 8px;
+}
 
-	.article-info div:first-child {
-		font-size: 13px;
-		color: #BBBBBB;
-	}
+.article-info div:first-child {
+	font-size: 13px;
+	color: #bbbbbb;
+}
 
-	.article-info div:last-child {
-		display: flex;
-		flex-direction: row;
-		justify-content: space-around;
-		align-items: center;
-		font-size: 14px;
-		color: #505050;
-		width: 30%;
-	}
+.article-info div:last-child {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-around;
+	align-items: center;
+	font-size: 14px;
+	color: #505050;
+	width: 30%;
+}
 
-	/*********** 热门 end ***********/
+/*********** 热门 end ***********/
 
-	/*********** 讨论区 start ***********/
-	.discuss-module {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		border-bottom: 10px solid #F5F6F6;
-	}
-	
-	.my-discuss {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		margin: 15px 10px;
-	}
-	
-	.my-discuss-top {
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-		align-items: center;
-	}
-	
-	.my-discuss-bottom {
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-		margin: 9px 0;
-	}
-	
-	.my-discuss-top div:first-child {
-		font-size: 16px;
-		color: #3A3B3B;
-		font-weight: 600;
-	}
-	
-	.my-discuss-top div:last-child {
-		font-size: 16px;
-		color: #C0C1C1;
-		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
-		align-items: center;
-	}
-	
-	.my-discuss-bottom-right {
-		text-align: left;
-		margin-right: 50px;
-		line-height: 27px;
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-	}
-	
-	.my-discuss-bottom-right div:first-child {
-		font-size: 17px;
-		color: #3A3B3B;
-		font-weight: 600;
-	}
-	
-	.my-discuss-bottom-right div:last-child {
-		font-size: 15px;
-		color: #B4B4B4;
-	}
-	
-	.my-discuss-bottom-right div:last-child span {
-		color: #F2C459;
-	}
-	
-	/*********** 讨论区 end ***********/
+/*********** 讨论区 start ***********/
+.discuss-module {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	/* border-bottom: 10px solid #f5f6f6; */
+}
 
-	/*********************** 文章模块 end ***********************/
+.my-discuss {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	margin: 15px 10px;
+}
+
+.my-discuss-top {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+}
+
+.my-discuss-bottom {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	margin: 9px 0;
+}
+
+.my-discuss-top div:first-child {
+	font-size: 16px;
+	color: #3a3b3b;
+	font-weight: 600;
+}
+
+.my-discuss-top div:last-child {
+	font-size: 16px;
+	color: #c0c1c1;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+}
+
+.my-discuss-bottom-right {
+	text-align: left;
+	margin-right: 50px;
+	line-height: 27px;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+}
+
+.my-discuss-bottom-right div:first-child {
+	font-size: 17px;
+	color: #3a3b3b;
+	font-weight: 600;
+}
+
+.my-discuss-bottom-right div:last-child {
+	font-size: 15px;
+	color: #b4b4b4;
+}
+
+.my-discuss-bottom-right div:last-child span {
+	color: #f2c459;
+}
+
+.wealth-team {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	margin: 8px 10px;
+}
+
+.my-wealt-bottom {
+	margin: 15px 0 5px;
+}
+
+/*********** 讨论区 end ***********/
+
+/*********************** 文章模块 end ***********************/
 </style>
--- src/views/Optional.vue
+++ src/views/Optional.vue
@@ -1,12 +1,75 @@
 <template>
-	<div class="optional">2</div>
+	<div class="optional">
+		<div class="optional-top">
+			<div class="optional-nav">
+				<div>市场</div>
+				<div>
+					<van-tabs type="card">
+						<van-tab title="标签 1" to="/"></van-tab>
+						<van-tab title="标签 2" to="/"></van-tab>
+					</van-tabs>
+				</div>
+				<div>
+					<van-icon name="search" />
+					<van-icon name="apps-o" />
+				</div>
+			</div>
+			<div class="optional-bar">
+				<van-sticky :offset-top="46">
+					<van-notice-bar left-icon="volume-o" :scrollable="false" color="#AEAEAE" background="#fff">
+						<van-swipe vertical class="optional-notice-swipe" :autoplay="3000" :show-indicators="false">
+							<van-swipe-item>内容 1</van-swipe-item>
+							<van-swipe-item>内容 2</van-swipe-item>
+							<van-swipe-item>内容 3</van-swipe-item>
+						</van-swipe>
+					</van-notice-bar>
+				</van-sticky>
+			</div>
+			<div class="optional-choice">
+				<van-sticky :offset-top="86">
+					<van-row>
+						<van-col span="4">全部</van-col>
+						<van-col span="4" offset="8">最新价</van-col>
+						<van-col offset="4" span="4" class="choice-tab">跌涨幅</van-col>
+					</van-row>
+				</van-sticky>
+			</div>
+		</div>
+	</div>
 </template>
 
-<script>
-</script>
+<script></script>
 
 <style>
-	.optional {
-		color: red;
-	}
+.optional-nav {
+	position: fixed;
+	top: 0;
+	width: 100%;
+	height: 46px;
+	background-color: #2671fe;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-around;
+	align-items: center;
+	color: #fff;
+}
+.optional-notice-swipe {
+	height: 30px;
+	line-height: 30px;
+}
+
+.optional-choice {
+	color:#AEAEAE;
+	font-size: 16px;
+	background-color: #F8F8FA;
+	line-height: 42px;
+}
+
+.choice-tab {
+	margin-left: 55px;
+}
+
+.optional-choice {
+	background-color: #F8F8FA;
+}
 </style>
